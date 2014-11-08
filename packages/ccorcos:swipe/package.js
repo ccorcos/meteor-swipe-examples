26df9157dd9dea377e363db358c78820951ace1b
Chet Corcos <ccorcos@gmail.com>
2014-11-07 16:21:59 -0800

    package creates

--- packages/ccorcos:swipe/package.js
+++ packages/ccorcos:swipe/package.js
@@ -1,17 +1,25 @@
 Package.describe({
   name: 'ccorcos:swipe',
-  summary: ' /* Fill me in! */ ',
+  summary: 'A package for creating apps that swipe between pages',
   version: '1.0.0',
   git: ' /* Fill me in! */ '
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('ccorcos:swipe.js');
+
+  api.use(['templating', 'coffeescript'], 'client')
+
+  api.addFiles(['swipe/swipe.coffee', 'swipe/swipe.html', 'swipe/swipe.css'], 'client');
+
+  if (api.export) {
+    api.export('Swipe')
+  }
+
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('ccorcos:swipe');
-  api.addFiles('ccorcos:swipe-tests.js');
-});
+// Package.onTest(function(api) {
+//   api.use('tinytest');
+//   api.use('ccorcos:swipe');
+//   api.addFiles('ccorcos:swipe-tests.js');
+// });
